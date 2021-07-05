2dd806056b16b57fd55c3f4cf09ece7fddc0cbd3
Adriel Tosi <dev.adrieltosi@gmail.com>
2021-07-05 09:30:34 +0200

    Add basic typography styles

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -3,7 +3,9 @@ import React from "react";
 export const Hello = () => {
   return (
     <div className="columns is-centered">
-      <div className="column is-2">test</div>
+      <div className="column is-2">
+        <h1>test</h1>
+      </div>
       <div className="column is-2">test</div>
     </div>
   );
