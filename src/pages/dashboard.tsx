94632896ba76fcb8e38bf5319ec919a31eb158af
Adriel Tosi <dev.adrieltosi@gmail.com>
2022-04-19 09:32:49 +0200

    Make home page into dashboard

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,22 +1,52 @@
 import React from "react";
-
-import { usePrivateRoute } from "lib/auth";
+import Link from "next/link";
+import { StoryCard } from "components/StoryCard";
 import { withApollo } from "apollo/withApollo";
-import { useAppLogout } from "lib/auth";
+import { GetServerSideProps, NextPage } from "next";
+import { ssrCurrentUser, ssrGetChaptersFromUser } from "graphql/generated/page";
+import { GetChaptersFromUserQuery } from "graphql/generated/graphqlTypes";
+import { ServerSideProps } from "types/ServerSideProps";
+import { usePrivateRoute } from "lib/auth";
 
-const Dashboard = () => {
-  usePrivateRoute();
-  const logout = useAppLogout();
+type HomeProps = ServerSideProps<GetChaptersFromUserQuery>
+
+const Dasboard: NextPage<HomeProps> = (props) => {
+  usePrivateRoute()
   return (
-    <div className="columns is-centered is-vcentered is-gapless is-full-height is-mobile">
-      <div className="column is-8">
-        <h1 className="has-text-weight-semibold">Dashboard</h1>
-        <button onClick={() => logout()}>Logout</button>
+    <div className="container">
+      <div className="columns is-multiline is-full-height">
+        {props.data.getChaptersFromUser.map(chapter =>
+          <div className="column is-3" key={chapter.id}>
+            <StoryCard title={chapter.title} description={chapter.description} published={chapter.createdAt} id={chapter.id} />
+          </div>
+        )}
       </div>
     </div>
   );
+}
+
+export default withApollo(Dasboard)
+
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const user = await ssrCurrentUser.getServerPage({
+    notifyOnNetworkStatusChange: true, context: {
+      headers: {
+        cookie: context.req.headers.cookie
+      }
+    }
+  }, context)
+
+  try {
+    const res = await ssrGetChaptersFromUser.getServerPage(
+      {
+        variables: { username: user.props.data.currentUser.username },
+        notifyOnNetworkStatusChange: true,
+      },
+      context
+    );
+    return res;
+  } catch (error: any) {
+    return { props: { error: error.message } };
+  }
 };
-
-
-
-export default withApollo(Dashboard);
